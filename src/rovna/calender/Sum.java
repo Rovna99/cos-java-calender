59709baedd030392ece8fdb928c98dc8f9c78d68
Rovna99 <tjdalswodi91@gmail.com>
2021-07-06 16:36:31 +0900

    ADD sum of two numbers

---
+++ src/rovna/calender/Sum.java
@@ -0,0 +1,19 @@
+package rovna.calender;
+
+import java.util.Scanner;
+
+public class Sum {
+	public static void main(String[] args) {
+		int a,b ;
+		Scanner scanner = new Scanner ( System.in);
+		String s1, s2;
+		System.out.println("두 수를 입력해주세요 ");
+		s1 = scanner.next();
+		s2 = scanner.next();
+		a =Integer.parseInt(s1);
+		b =Integer.parseInt(s2);
+		System.out.printf("두 수의 합은 %d 입니다", a+b);
+		scanner.close();
+
+	};
+}
